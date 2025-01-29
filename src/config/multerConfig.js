c2967f829fc56e8cc9aac9e2fe06f037695ed0c0
adityaraj9983@gmail.com <adityaraj9983@gmail.com>
2025-01-29 22:29:25 +0530

    comment added

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -16,17 +16,17 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  
+//   check for undefined
     if (!file) {
       return cb(new Error("No file uploaded"), false);
     }
-  
+//   check for file type
     const allowedMimeTypes = ["image/png", "image/jpg", "image/jpeg", "application/pdf"];
   
     if (allowedMimeTypes.includes(file.mimetype)) {
       cb(null, true); // Accept file
     } else {
-      cb(new Error("Only .png, .jpg, .jpeg, and .pdf formats are allowed!"), false);
+      cb(new Error("Only .png, .jpg, .jpeg, and .pdf formats are allowed!"), false); // Reject if unknown file type
     }
   };
   
